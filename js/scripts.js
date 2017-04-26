5b7c66bf00d0f9a527c5bf2262d74949b9ea19a8
Joe Parsley and Karina Roush <joe@>
2017-04-26 16:24:47 -0700

    add beginning of javascript for clothing shop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-$(.document).ready(function(){
-  var shirtStyle = $("#shirtStyle").val();
+var getFullContactInfo = function(name, address, phone){
+  return "<p>name:" + name+"</p>";
+}
+
+
+
+$(document).ready(function(){
+
+  $("form#customer").submit(function(event) {
+    event.preventDefault();
+    var shirtStyle = $("#shirtStyle").val();
+    var shirtSize = $("input:radio[name=size]:checked").val();
+    var shirtColor = $("#shirtColor").val();
+    var graphic = $("input:radio[name=graphic]:checked").val();
+    var customerName = $("input#name").val();
+
+    var fullContactInfo = getFullContactInfo(customerName);
+
+    $("#output").text(fullContactInfo);
+  });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+  // var result = $("#shirtStyle").val();
+  // $("#output") var result;
